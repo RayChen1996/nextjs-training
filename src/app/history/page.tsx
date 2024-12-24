5ad79b8c7bfe7c92cd259b5b4c6cb04331b376ed
ray <53119155+RayChen1996@users.noreply.github.com>
2024-12-24 21:38:17 +0800

    Initial commit

---
+++ src/app/history/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Page() {
+  return <div>歷史紀錄</div>;
+}
---
+++ src/components/Layouts/AppSide.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+import {
+  Sidebar,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarFooter,
+} from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown, ChevronUp, User2 } from "lucide-react";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+const items = [
+  {
+    title: "使用者",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "歷史紀錄",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "卡鐘設定",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "資料查詢",
+    url: "/history",
+    icon: Search,
+  },
+  {
+    title: "系統設定",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export default function AppSide() {
+  return (
+    <div>
+      <Sidebar collapsible="offcanvas">
+        <SidebarHeader>
+          {/* <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    Select Workspace
+                    <ChevronDown className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+                  <DropdownMenuItem>
+                    <span>Acme Inc</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <span>Acme Corp.</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu> */}
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Application</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    <User2 /> Username
+                    <ChevronUp className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[--radix-popper-anchor-width]"
+                >
+                  <DropdownMenuItem>
+                    <span>Account</span>
+                  </DropdownMenuItem>
+
+                  <DropdownMenuItem>
+                    <span>Sign out</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+    </div>
+  );
+}
